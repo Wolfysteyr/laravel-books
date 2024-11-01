1f6de43732173b6fc6008423502739107d2a0684
Deivis <abc@abc.com>
2024-11-01 12:52:53 +0200

    did allat

---
+++ resources/views/book/create.blade.php
@@ -0,0 +1,2 @@
+<div>
+<h1>create</h1></div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/book', [BookController::class, 'index'])->name('book.index');
+Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
+Route::post('/book', [BookController::class, 'store'])->name('book.store');
+Route::get('/book/{id}', [BookController::class, 'show'])->name('book.show');
+Route::get('/book/{id}/edit', [BookController::class, 'edit'])->name('book.edit');
+Route::put('/book/{id}', [BookController::class, 'update'])->name('book.update');
+Route::delete('/book/{id}', [BookController::class, 'destroy'])->name('book.destroy');
